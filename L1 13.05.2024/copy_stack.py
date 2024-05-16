d7bea89c81c8b0f8277c4d2cbea58bfc5a093ce9
Stanisław <s.piecho09@gmail.com>
2024-05-16 15:47:37 +0200

    infix to postfix

--- L1 13.05.2024/copy_stack.py
+++ L1 13.05.2024/copy_stack.py
@@ -7,50 +7,50 @@ class Stack:
         self.index = -1
 
     def push(self, value: int) -> bool:
-        if (self.index >= MAXIMUM_LENGTH - 1):
+        if self.index >= MAXIMUM_LENGTH - 1:
             return False
-        
+
         self.index += 1
         self.values[self.index] = value
         return True
-    
+
     def pop(self) -> int:
         assert not self.is_empty()
         self.index -= 1
         return self.values[self.index + 1]
-    
+
     def is_empty(self) -> bool:
         return self.index <= -1
-    
+
     def top(self) -> int:
         assert not self.is_empty()
         return self.values[self.index]
-    
+
     def __repr__(self):
         return "[" + ' '.join([str(x) for x in self.values]) + "]"
-    
+
 
 def test_stack():
-    test_stack = Stack()
+    my_stack = Stack()
 
     try:
-        print(f"Last value: {test_stack.pop()}")
+        print(f"Last value: {my_stack.pop()}")
     except AssertionError as error:
         print(error)
         print("Blocking invalid operation is working")
 
     i = 0
-    while test_stack.push(i):
+    while my_stack.push(i):
         i += 1
     print(f"Push nr {i+1} returned not successful")
-    print(test_stack)
+    print(my_stack)
 
-    while not test_stack.is_empty():
-        print(f"The last element: {test_stack.top()}, index pointing to {test_stack.index}")
-        print(f"Removed last element: {test_stack.pop()}, index pointing to {test_stack.index}")
-        print(f"New last element: {test_stack.top()}, index pointing to {test_stack.index}")
+    while not my_stack.is_empty():
+        print(f"The last element: {my_stack.top()}, index pointing to {my_stack.index}")
+        print(f"Removed last element: {my_stack.pop()}, index pointing to {my_stack.index}")
+        print(f"New last element: {my_stack.top()}, index pointing to {my_stack.index}")
         print("\n\n")
 
 
 if __name__ == "__main__":
-    pass
+    infix = input("> ")
--- L1 13.05.2024/stack.cpp
+++ L1 13.05.2024/stack.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <assert.h>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
 
 #define MAX_SIZE 100
 #define EMPTY -1
@@ -7,11 +11,11 @@
 class Stack
 {
     public:
-    int values[MAX_SIZE];
+    int values[MAX_SIZE - 1];
     int index = EMPTY;
 
     bool push(int value){
-        if (index >= MAX_SIZE)
+        if (index >= MAX_SIZE - 1)
             return false;
 
         values[++index] = value;
@@ -31,15 +35,79 @@ class Stack
         assert(!isEmpty());
         return *(values + index);
     }
+
+    char topc(){
+        return (char) top();
+    }
 };
+
+int get_priority(char c){
+    switch (c){
+        case '(':
+            return 4;
+            break;
+        case '+':
+            return 3;
+            break;
+        case '-':
+            return 3;
+            break;
+        case '*':
+            return 2;
+            break;
+        case '/':
+            return 2;
+            break;
+        case ')':
+            return 1;
+            break;
+    }
+
+    return 0;
+}
+
+void cleanOperators(Stack* operators, string* postfix){
+    while (!operators->isEmpty() && (operators->topc() != '(')){
+        *postfix += operators->pop();
+        *postfix += ' ';
+    }
+    if (!operators->isEmpty())
+        operators->pop();
+}
 
 int main(int argc, char const *argv[])
 {
-    Stack test_stack = Stack();
+    Stack operators;
+    string line, postfix;
 
-    for (int i=1; i<=100; i++){
-        test_stack.push(i);
+    // getline(cin, line);
+    line = "5 * ( 3 * 6 + 2 ) + 10 + ( 14 / 3 + 4 )";
+    for (size_t i=0; i<line.length(); i++){
+        if (line[i] == ' ')
+            continue;
+        
+        if (isdigit(line[i])){
+            postfix += line[i];
+            if(!isdigit(line[i+1]))
+                postfix += ' '; 
+        } else {
+            if (!operators.isEmpty() && (get_priority(line[i]) >= get_priority(operators.topc()))){
+                operators.push((int) line[i]);
+            } else if (operators.isEmpty()){
+                cout << "Empty stack" << endl;
+                operators.push((int) line[i]);
+            } else if (operators.topc() != '(') {
+                cout << "Cleaning operators" << endl;
+                cleanOperators(&operators, &postfix);
+                if (line[i] != ')')
+                    operators.push((int) line[i]);
+            } else {
+                operators.push((int) line[i]);
+            }
+        }
     }
+    cleanOperators(&operators, &postfix);
 
+    cout << "The result: " << postfix << endl;
     return 0;
 }
